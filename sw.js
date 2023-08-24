93c8a3cc7e4c413e23d4da0f41d9d4d86a5571b2
Felipe Stroff <stroff.felipe@gmail.com>
2023-08-24 10:22:08 -0300

    Service worker refatorado e documentado

--- sw.js
+++ sw.js
@@ -1,18 +1,43 @@
-// https://willianjusten.com.br/como-fazer-seu-site-funcionar-offline-com-pwa
+/**
+ * Service Worker for the bird-finder application.
+ * 
+ * This Service Worker handles caching of assets to make
+ * the application work offline and improve load performance.
+ * 
+ * Last Modified: 08/24/2023
+ * 
+ * @see https://willianjusten.com.br/como-fazer-seu-site-funcionar-offline-com-pwa
+ */
 
-// Choose a cache name
+// Cache Versioning
 const CACHE_VERSION = 'v1';
 const CACHE_NAME = `bird-finder-${CACHE_VERSION}`;
 
-const precacheResources = [
+// Core Resources: Essential for the functioning of the app.
+const coreResources = [
   './',
   './index.html',
   './manifest.json',
-  './config/config.json',
-  './locales/pt-BR.json',
+];
+
+// JavaScript Resources: Application logic.
+const jsResources = [
   './js/main.js',
   './js/app.js',
+];
+
+// CSS Resources: Styling information.
+const cssResources = [
   './css/style.css',
+  './libs/leaflet-draw/leaflet.draw.css',
+  './libs/leaflet-markercluster/MarkerCluster.css',
+  './libs/leaflet-markercluster/MarkerCluster.Default.css',
+  './libs/bootstrap/bootstrap.css',
+  './libs/leaflet/leaflet.css',
+];
+
+// Image Resources: Visual assets used across the app.
+const imageResources = [
   './assets/favicon.ico',
   './assets/favicon-16x16.png',
   './assets/favicon-32x32.png',
@@ -26,76 +51,120 @@ const precacheResources = [
   './assets/en-US.png',
   './assets/pt-BR.png',
   './assets/sound.png',
-  './libs/bootstrap/bootstrap.css',
-  './libs/bootstrap/bootstrap.bundle.js',
-  './libs/leaflet/leaflet.css',
-  './libs/leaflet/leaflet.js',
   './libs/leaflet/images/layers.png',
   './libs/leaflet/images/layers-2x.png',
   './libs/leaflet/images/marker-icon.png',
   './libs/leaflet/images/marker-icon-2x.png',
   './libs/leaflet/images/marker-shadow.png',
   './libs/leaflet-draw/images/spritesheet.svg',
-  './libs/leaflet-draw/leaflet.draw.css',
+];
+
+// Library Resources: Third-party libraries and data.
+const libraryResources = [
+  './libs/bootstrap/bootstrap.bundle.js',
+  './libs/leaflet/leaflet.js',
   './libs/leaflet-draw/leaflet.draw.js',
   './libs/leaflet-markercluster/leaflet.markercluster.js',
-  './libs/leaflet-markercluster/MarkerCluster.css',
-  './libs/leaflet-markercluster/MarkerCluster.Default.css'
-]
+  './config/config.json',
+  './locales/pt-BR.json',
+];
 
+// Consolidated List of All Resources to Cache
+const precacheResources = [
+  ...coreResources,
+  ...jsResources,
+  ...cssResources,
+  ...imageResources,
+  ...libraryResources
+];
+
+/**
+ * Logging Constants: These constants provide a unified format for log messages, 
+ * making it easier to identify and manage logs related to the Service Worker.
+ */
+const LOG_PREFIX = '[Service Worker]';
+const LOG_INSTALL = `${LOG_PREFIX} Install event!`;
+const LOG_ACTIVATE = `${LOG_PREFIX} Activate event!`;
+const LOG_FETCH = `${LOG_PREFIX} Fetch intercepted for:`;
+const LOG_FAILED_CACHE_OPEN = `${LOG_PREFIX} Failed to open cache.`;
+const LOG_FAILED_CACHE_RESOURCE = `${LOG_PREFIX} Failed to cache resource.`;
+const LOG_FAILED_DELETE_OLD_CACHES = `${LOG_PREFIX} Failed to delete old caches.`;
+const LOG_FAILED_FETCH_CACHE = `${LOG_PREFIX} Error fetching from cache.`;
+
+/**
+ * Event Listener: Installation
+ * Cache necessary resources during Service Worker installation.
+ */
 self.addEventListener('install', (event) => {
-  console.log('[sw] Install event!');
-
+  console.log(LOG_INSTALL);
   self.skipWaiting();
-
-  const filesUpdate = (cache) => {
-    const promises = precacheResources.map((resource) => {
-      return cache.add(resource).catch((error) => {
-        console.error(`[sw] Failed to cache resource: ${resource}`, error);
-      });
-    });
-
-    return Promise.all(promises);
-  };
-
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(filesUpdate)
-      .catch((error) => {
-        console.error('[sw] Failed to open cache:', error);
-      })
-  );
+  event.waitUntil(cacheFiles());
 });
 
+/**
+ * Event Listener: Activation
+ * Cleanup old caches on Service Worker activation.
+ */
 self.addEventListener('activate', (event) => {
-  console.log('[sw] Activate event!');
-
-  event.waitUntil(
-    caches.keys()
-      .then((cacheNames) => {
-        return Promise.all(
-          cacheNames
-            .filter((cacheName) => cacheName.startsWith('cache-'))
-            .filter((cacheName) => cacheName !== CACHE_NAME)
-            .map((cacheName) => caches.delete(cacheName))
-        );
-      })
-      .catch((error) => {
-        console.error('[sw] Failed to delete old caches:', error);
-      })
-  );
+  console.log(LOG_ACTIVATE);
+  event.waitUntil(cleanupOldCaches());
 });
 
+/**
+ * Event Listener: Fetch
+ * Intercept network requests and serve them from cache if available.
+ */
 self.addEventListener('fetch', (event) => {
-  console.log('[sw] Fetch intercepted for:', event.request.url);
-
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
-      .catch((error) => {
-        console.error('[sw] Error fetching from cache:', error);
-      })
-  );
+  console.log(`${LOG_FETCH} ${event.request.url}`);
+  event.respondWith(fetchFromCacheOrNetwork(event.request));
 });
+
+/**
+ * Cache Files: Function to cache the defined resources.
+ * @returns {Promise}
+ */
+async function cacheFiles() {
+  try {
+    const cache = await caches.open(CACHE_NAME);
+    await Promise.all(precacheResources.map(resource => 
+      cache.add(resource).catch(error => 
+        console.error(`${LOG_FAILED_CACHE_RESOURCE} ${resource}`, error)
+      )
+    ));
+  }
+  catch (error) {
+    console.error(LOG_FAILED_CACHE_OPEN, error);
+  }
+}
+
+/**
+ * Cleanup Old Caches: Function to remove outdated caches.
+ * @returns {Promise}
+ */
+async function cleanupOldCaches() {
+  try {
+    const cacheNames = await caches.keys();
+    const oldCacheNames = cacheNames
+      .filter(cacheName => cacheName.startsWith('cache-') && cacheName !== CACHE_NAME);
+    await Promise.all(oldCacheNames.map(cacheName => caches.delete(cacheName)));
+  }
+  catch (error) {
+    console.error(LOG_FAILED_DELETE_OLD_CACHES, error);
+  }
+}
+
+/**
+ * Fetch from Cache or Network: 
+ * Function to first try fetching a resource from cache, and if unavailable, fetch from the network.
+ * @param {Request} request - The request object.
+ * @returns {Promise}
+ */
+async function fetchFromCacheOrNetwork(request) {
+  try {
+    const cachedResponse = await caches.match(request);
+    return cachedResponse || await fetch(request);
+  }
+  catch (error) {
+    console.error(LOG_FAILED_FETCH_CACHE, error);
+  }
+}
