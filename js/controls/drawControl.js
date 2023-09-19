cefd91f36c209c202704cef996c1c89224c7a513
Felipe Stroff <stroff.felipe@gmail.com>
2023-09-19 14:14:52 -0300

    Refatoracao do controle de desenho

--- js/controls/drawControl.js
+++ js/controls/drawControl.js
@@ -43,61 +43,73 @@ export default class DrawControl {
      * @private
      */
     _setControlLocale() {
-        const local = L.drawLocal;
+        const { drawLocal: local } = L;
+        const translate = (key) => this.langControl.translate(key);
 
-        // Circle configurations
-        local.draw.handlers.circle.radius = this.langControl.translate('Radius');
-        local.draw.handlers.circle.tooltip.start = this.langControl.translate('Click and drag to draw circle');
+        // Handlers configuration
+        Object.assign(local.draw.handlers, {
+            circle: {
+                radius: translate('Radius'),
+                tooltip: { start: translate('Click and drag to draw circle') },
+            },
+            polygon: {
+                tooltip: {
+                    start: translate('Click to start drawing shape'),
+                    cont: translate('Click to continue drawing shape'),
+                    end: translate('Click first point to close this shape'),
+                },
+            },
+            rectangle: {
+                tooltip: { start: translate('Click and drag to draw rectangle') },
+            },
+            simpleshape: {
+                tooltip: { end: translate('Release to finish drawing') },
+            },
+        });
 
-        // Polygon configurations
-        local.draw.handlers.polygon.tooltip = {
-            cont: this.langControl.translate('Click to continue drawing shape'),
-            end: this.langControl.translate('Click first point to close this shape'),
-            start: this.langControl.translate('Click to start drawing shape')
-        };
-
-        // Rectangle configurations
-        local.draw.handlers.rectangle.tooltip.start = this.langControl.translate('Click and drag to draw rectangle');
-        
-        // Simple shape configurations
-        local.draw.handlers.simpleshape.tooltip.end = this.langControl.translate('Release to finish drawing');
-
-        // Toolbar button configurations
-        local.draw.toolbar.buttons = {
-            polygon: this.langControl.translate('Draw a polygon'),
-            rectangle: this.langControl.translate('Draw a rectangle'),
-            circle: this.langControl.translate('Draw a circle')
-        };
-
-        // Toolbar action configurations
-        Object.assign(local.draw.toolbar.actions, {
-            text: this.langControl.translate('Cancel'),
-            title: this.langControl.translate('Cancel drawing'),
+        // Toolbar configuration
+        Object.assign(local.draw.toolbar, {
+            buttons: {
+                polygon: translate('Draw a polygon'),
+                rectangle: translate('Draw a rectangle'),
+                circle: translate('Draw a circle'),
+            },
             finish: {
-                text: this.langControl.translate('Finish'),
-                title: this.langControl.translate('Finish drawing')
+                text: translate('Finish'),
+                title: translate('Finish drawing'),
             },
             undo: {
-                text: this.langControl.translate('Delete last point'),
-                title: this.langControl.translate('Delete last point drawn')
-            }
+                text: translate('Delete last point'),
+                title: translate('Delete last point drawn'),
+            },
+            actions: {
+                text: translate('Cancel'),
+                title: translate('Cancel drawing'),
+                finish: {
+                    text: translate('Finish'),
+                    title: translate('Finish drawing'),
+                },
+                undo: {
+                    text: translate('Delete last point'),
+                    title: translate('Delete last point drawn'),
+                },
+            },
         });
 
-        // Edit toolbar configurations
+        // Edit toolbar and handler configuration
         Object.assign(local.edit.toolbar.actions, {
             cancel: {
-                text: this.langControl.translate('Cancel'),
-                title: this.langControl.translate('Cancel editing, discards all changes')
+                text: translate('Cancel'),
+                title: translate('Cancel editing, discards all changes'),
             },
             clearAll: {
-                text: this.langControl.translate('Clear All'),
-                title: this.langControl.translate('Clear all layers')
+                text: translate('Clear All'),
+                title: translate('Clear all layers'),
             },
-            removeDisabled: this.langControl.translate('No layers to delete'),
-            remove: this.langControl.translate('Delete layers')
+            removeDisabled: translate('No layers to delete'),
+            remove: translate('Delete layers'),
         });
 
-        // Edit handler configurations
-        local.edit.handlers.remove.tooltip.text = this.langControl.translate('Click on a feature to remove');
+        local.edit.handlers.remove.tooltip.text = translate('Click on a feature to remove');
     }
 }
